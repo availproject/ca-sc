220883af88a2963f71826c4454a5cc3cf24f3024
mmjee <root@git.maharshi.ninja>
2025-09-24 14:58:05 +0530

    ERC-7914 sweeping

--- contracts/ERC20Sweeper.sol
+++ contracts/ERC20Sweeper.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.29;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+interface IERC7914 {
+    function transferFromNative(address from, address recipient, uint256 amount) external returns (bool);
+}
+
 contract ERC20Sweeper {
     using SafeERC20 for IERC20;
 
@@ -13,5 +17,16 @@ contract ERC20Sweeper {
             return;
         }
         token.safeTransferFrom(msg.sender, to, bal);
+    }
+
+    function sweepERC7914(address user, address to) external {
+        uint256 bal = user.balance;
+        if (bal == 0) {
+            return;
+        }
+        bool ok = IERC7914(user).transferFromNative(user, to, bal);
+        if (!ok) {
+            revert("ERC7914 transferFromNative reverted");
+        }
     }
 }
