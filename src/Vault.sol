a81612a0fa6895c1b99722f5973b702f478cda07
rac-sri <rac.sri25@gmail.com>
2026-07-20 16:39:57 +0530

    fix: deposit mayan route lock

--- src/Vault.sol
+++ src/Vault.sol
@@ -34,7 +34,9 @@ contract Vault is Initializable, UUPSUpgradeable, AccessControlUpgradeable, Reen
 
     bytes32 private constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 private constant SETTLEMENT_VERIFIER_ROLE = keccak256("SETTLEMENT_VERIFIER_ROLE");
+    bytes32 private constant MIDDLEWARE_ROLE = keccak256("MIDDLEWARE_ROLE");
     string private constant SIGNATURE_PREFIX = "Sign this intent to proceed \n";
+
     // Storage gap to reserve slots for future use
     uint256[49] private _gap;
 
@@ -163,6 +165,8 @@ contract Vault is Initializable, UUPSUpgradeable, AccessControlUpgradeable, Reen
         require(!depositNonce[request.nonce], "Vault: Nonce already used");
         require(!depositNonce[depositKey],"Vault: Deposit Key based nonce already used");
         require(request.expiry > block.timestamp, "Vault: Request expired");
+
+        require(msg.sender == from || hasRole(MIDDLEWARE_ROLE, msg.sender), "Invalid Sender");
 
         depositNonce[depositKey] = true;
         requestState[signedMessageHash] = RFFState.DEPOSITED;
